0f51c78baa1d9ad3bc7072ea1b35d748f8764e3b
MuskanGupta2702 <83824990+MuskanGupta2702@users.noreply.github.com>
2021-06-23 14:07:53 +0530

    make navbar width 80% in mobile view

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// document.getElementById("ham").addEventListener("click",function(){console.log("hello")})
 document.getElementById("ham").addEventListener("click",openNav)
 document.getElementById("btn").addEventListener("click",closeNav)
-// let x = document.getElementsByTagName("body")
-// console.log(x)
 if(window.screen.width <= 500)
 {
     let y = document.getElementsByClassName("item")
@@ -12,17 +9,13 @@ y[0].addEventListener("click",closeNav)
 y[1].addEventListener("click",closeNav)
 y[2].addEventListener("click",closeNav)
 y[3].addEventListener("click",closeNav)
-    // let i
-    // for(i in y){
-    //     console.log(y[i])
-    //     y[i].addEventListener("click",closeNav)
-    
 }
 
 function openNav()
 {
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.width = "80%";
 }
 function closeNav(){
     document.getElementById("mySidenav").style.width = "0";
 }
+
